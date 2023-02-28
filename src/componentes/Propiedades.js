16990cc68a3073ce0803df6f04d87fc620b9cced
Sergio Pichardo <ing.sergio.pichardo.a@gmail.com>
2023-02-27 19:35:27 -0600

    se cambiaron los testimonios con sus props  de app a un nuevo componente para tener mejor estetica en el documento

---
+++ src/componentes/Propiedades.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Testimonio from './Testimonio'
+
+
+function Propiedades () { 
+    
+  return (
+    <div className="testimonioss">
+      <Testimonio  
+        nombre='Shawn Wang '
+        pais='Singapur'
+        imagen='Shawn'
+        cargo='Ingeniero de software'
+        empresa='Amazon'  
+        testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida'/>
+
+      <Testimonio
+        nombre='Sara Chima'
+        pais='Nigeria'
+        imagen='Sarah'
+        cargo='Ingeniero de software'
+        empresa='ChatDesk'  
+        testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble'/>
+     
+      <Testimonio
+        nombre='Emma Bostian'
+        pais='Suecia'
+        imagen='Emma'
+        cargo='Ingeniero de software'
+        empresa='Spotify'
+        testimonio='Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify'/>
+    </div>
+    );
+}
+
+export default Propiedades;
